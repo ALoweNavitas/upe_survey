4ab921a270bf54d465500aad7acfa1442bdab7fc
ALoweNavitas <adam.lowe@navitas.com>
2022-01-19 15:44:24 +0000

    Working concept, but missing text answers

--- upe_survey_download.py
+++ upe_survey_download.py
@@ -7,8 +7,7 @@ import update_survey
 access_token = os.environ.get('access_token_surveymonkey')
 survey_id = 315864260
 
-# update_survey.update_survey_data(survey_id=survey_id, access_token=access_token)
-# update_survey.update_survey_details(survey_id=survey_id, access_token=access_token)
+update_survey.update_survey_data(survey_id=survey_id, access_token=access_token)
 
 with open('data.json', 'r') as data_file:
     json_data = json.load(data_file)
@@ -20,13 +19,69 @@ def parse_timestamp(text):
     d = text.replace('+00:00', '')
     return datetime.strptime(d, '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
 
+# Map Question ID to Question Text
+id = pd.json_normalize(json_details['pages'], ['questions'])['id']
+question_text = pd.json_normalize(json_details['pages'], ['questions', 'headings'])['heading']
+questions = dict(zip(id, question_text))
+
+# Map ID to college variable
+id = []
+college_value = []
 for i in range(len(json_data['data'])):
-    print(json_data['data'][i]['custom_variables']['College'])
-    print(json_data['data'][i]['id'])
-    # print(json_data['data'][i]['custom_variables']['College'])
+    id.append(json_data['data'][i]['id'])
+    college_value.append(json_data['data'][i]['custom_variables']['College'])
 
-json_df = pd.json_normalize(data=json_data['data'],
-                        record_path=['pages', 'questions', 'answers'], # Whatever paths are here dictates what fields need to be collected in the meta code below. Can be str or list-like
-                        meta=['id', ['pages', 'questions', 'id'], ['answers', 'id']])
+colleges = dict(zip(id, college_value))
 
-print(json_df)
+# Map ID to semester variable
+semester_value = []
+for i in range (len(json_data['data'])):
+    semester_value.append(json_data['data'][i]['custom_variables']['Semester'])
+
+semesters = dict(zip(id, semester_value))
+
+# Map ID to program variable
+program_value = []
+for i in range(len(json_data['data'])):
+    program_value.append(json_data['data'][i]['custom_variables']['Program'])
+
+programs = dict(zip(id, program_value))
+
+# Map ID to module variable
+module_value = []
+for i in range (len(json_data['data'])):
+    module_value.append(json_data['data'][i]['custom_variables']['Module'])
+
+modules = dict(zip(id, module_value))
+
+# Building the dataframe
+json_df = pd.json_normalize(json_data['data'], ['pages', 'questions', 'answers'], ['id', ['questions', 'answers', 'id']])
+json_df['college'] = json_df['id'].map(colleges)
+json_df['semester'] = json_df['id'].map(semesters)
+json_df['program'] = json_df['id'].map(programs)
+json_df['module'] = json_df['id'].map(modules)
+json_df['question'] = json_df['questions.answers.id'].map(questions)
+
+# Rename columns
+json_df.rename(columns={
+    "choice_metadata.weight" : "answer",
+    "questions.answers.id" : "question_id",
+    "id" : "respondent_id"
+}, inplace=True)
+
+# Reshaping
+json_df = json_df[[
+    'respondent_id',
+    'college',
+    'semester',
+    'program',
+    'module',
+    'question_id',
+    'question',
+    'answer'
+]]
+
+# Questions as headers - values / index / column headers
+df = json_df.pivot_table('answer', ['respondent_id', 'college', 'program', 'semester', 'module'], 'question')
+df.to_excel('response_data.xlsx')
+print(df)
