2fabea70b8d7966c045d2c33d32ccbfa1dea664f
ALoweNavitas <adam.lowe@navitas.com>
2022-01-24 13:20:39 +0000

    fix: api calls are not executed in the same script

--- update_survey.py
+++ update_survey.py
@@ -1,6 +1,7 @@
 import http.client
 import json
 import os
+import time
 
 def main():
     pass
@@ -21,14 +22,17 @@ def update_survey_details(survey_id, access_token):
     conn = http.client.HTTPSConnection("api.surveymonkey.com")
     headers = {
         'Accept': "application/json",
-        'Authorization': "Bearer {}".format(access_token)
+        'Authorization': f"Bearer {access_token}"
         }
 
     conn.request("GET", "/v3/surveys/{}/details".format(survey_id), headers=headers)
     res = conn.getresponse()
     surveyDetails = res.read()
     json_data = json.loads(surveyDetails)
-
+        
+    if res.status >= 400: 
+        raise RuntimeError(f'Request failed: {res.status}')
+    
     # Write the JSON response to a file
     with open('details.json', 'w') as f:
         data = json.dump(json_data, f, indent=4, sort_keys=True)
@@ -42,13 +46,16 @@ def update_survey_data(survey_id, access_token):
     conn = http.client.HTTPSConnection("api.surveymonkey.com")
     headers = {
         'Accept': "application/json",
-        'Authorization': "Bearer {}".format(access_token)
+        'Authorization': f"Bearer {access_token}"
         }
 
     conn.request("GET", "/v3/surveys/{}/responses/bulk".format(survey_id), headers=headers)
     res = conn.getresponse()
     surveyDetails = res.read()
     json_data = json.loads(surveyDetails)
+
+    if res.status >= 400: 
+        raise RuntimeError(f'Request failed: {res.status}')
 
     # Write the JSON response to a file
     with open('data.json', 'w') as f:
@@ -62,13 +69,16 @@ def get_surveys(access_token):
     conn = http.client.HTTPSConnection("api.surveymonkey.com")
     headers = {
         'Accept': "application/json",
-        'Authorization': "Bearer {}".format(access_token)
+        'Authorization': f"Bearer {access_token}"
         }
 
     conn.request("GET", "/v3/surveys", headers=headers)
     res = conn.getresponse()
     surveyDetails = res.read()
     json_data = json.loads(surveyDetails)
+
+    if res.status >= 400:
+        raise RuntimeError(f'Request failed: {res.status}')
 
     # Write the JSON response to a file
     with open('surveys.json', 'w') as f:
@@ -77,5 +87,3 @@ def get_surveys(access_token):
 # get_surveys(access_token = access_token)
 # update_survey_details(survey_id = id, access_token = access_token)
 # update_survey_data(survey_id = id, access_token = access_token)
-
-
--- upe_survey_download.py
+++ upe_survey_download.py
@@ -1,13 +1,9 @@
 import pandas as pd 
+import http.client
 import os
 import json
-from datetime import date, datetime
-import update_survey
 import time
-
-#  Todo: 
-#  - Add the survey update functions to this code instead of having a seperate script;
-#  - Add exception messages to print to console.
+from datetime import date, datetime
 
 def main():
     pass
@@ -15,22 +11,73 @@ def main():
 if __name__ == '__main__':
     main()
 
-start_time = time.time()
+# Variables
 access_token = os.environ.get('access_token_surveymonkey')
 survey_id = 315864260
+start_time = time.time()
 
-update_survey.update_survey_data(survey_id=survey_id, access_token=access_token)
-update_survey.update_survey_details(survey_id=survey_id, access_token=access_token)
+# Functions
+def parse_timestamp(text):
+    d = text.replace('+00:00', '')
+    return datetime.strptime(d, '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
 
+def update_survey_details(survey_id, access_token):
+    '''
+    Calls the SurveyMonkey API to get the details of given survey, including the questions and general structure.
+    survey_id = 12345678
+    access_token = API access token from SurveyMonkey
+    '''
+    conn = http.client.HTTPSConnection("api.surveymonkey.com")
+    headers = {
+        'Accept': "application/json",
+        'Authorization': f"Bearer {access_token}"
+        }
+
+    conn.request("GET", "/v3/surveys/{}/details".format(survey_id), headers=headers)
+    res = conn.getresponse()
+    survey_details = res.read()
+    json_details = json.loads(survey_details)
+        
+    if res.status >= 400: 
+        raise RuntimeError(f'Request failed: {res.status}')
+    
+    # Write the JSON response to a file
+    with open('details.json', 'w') as f:
+        data = json.dump(json_details, f, indent=4, sort_keys=True)
+
+def update_survey_data(survey_id, access_token):
+    '''
+    Calls the SurveyMonkey API to get all of the data of a given survey.
+    survey_id = 12345678
+    access_token = API access token from SurveyMonkey
+    '''
+    conn = http.client.HTTPSConnection("api.surveymonkey.com")
+    headers = {
+        'Accept': "application/json",
+        'Authorization': f"Bearer {access_token}"
+        }
+
+    conn.request("GET", "/v3/surveys/{}/responses/bulk".format(survey_id), headers=headers)
+    res = conn.getresponse()
+    survey_data = res.read()
+    json_data = json.loads(survey_data)
+
+    if res.status >= 400: 
+        raise RuntimeError(f'Request failed: {res.status}')
+
+    # Write the JSON response to a file
+    with open('data.json', 'w') as f:
+        data = json.dump(json_data, f, indent=4, sort_keys=True)
+
+update_survey_details(access_token=access_token, survey_id=survey_id)
+update_survey_data(access_token=access_token, survey_id=survey_id)
+
+# Open the files 
 with open('data.json', 'r') as data_file:
     json_data = json.load(data_file)
 
 with open('details.json') as details_file: 
     json_details = json.load(details_file)
-
-def parse_timestamp(text):
-    d = text.replace('+00:00', '')
-    return datetime.strptime(d, '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
 
 # Map Question ID to Question Text
 id = pd.json_normalize(json_details['pages'], ['questions'])['id']
